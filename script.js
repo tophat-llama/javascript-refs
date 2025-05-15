10ff14e75bee4e5a125dc510445afcd06602b729
tophat-llama <dod.avalon.1632@gmail.com>
2025-05-15 11:58:19 +0100

    Made them changes, yo
    made some changes, yo.

---
+++ script.js
@@ -0,0 +1,34 @@
+// const multilineSentence = "this is a \n sentence \n wow another line"
+
+// console.log(multilineSentence)
+
+// const insertedVariable = "THIS";
+// console.log("This is an " + insertedVariable);
+
+// console.log(multilineSentence)
+
+// const variableSentence = `This is an ${insertedVariable}`;
+
+// console.log(variableSentence.length)
+
+// let userInput = "hEllo bOhDAN";
+
+// let fullSentence = "TEST TEST TEST BLAH BLAH SENTENCEY SENTENCE TIME"
+// let lengthSentence = fullSentence.length - 1;
+// let variableCase = fullSentence[lengthSentence];
+// console.log(lengthSentence);
+// console.log(variableCase);
+// console.log(fullSentence);
+
+// let userPrompt = prompt("Choose a Number :)");
+// console.log("userPrompt = " + userPrompt);
+// console.log("userPrompt type = " typeof userPrompt);
+
+// console.log(parseFloat(userPrompt));
+
+// console.log(parseInt(userPrompt));
+
+// const remainder = 10 % 2;
+// console.log(remainder);
+
+let useMath = Math.ceil(4.2);
